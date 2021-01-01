eea79e79cffa44ad21f92f7d8839a37836255ae7
atabek-bakh <coderonline@icloud.com>
2021-01-01 19:31:43 +0100

    deleted tags from index.html and created them in the DOM

--- Week2/homework/pokemon-app/script.js
+++ Week2/homework/pokemon-app/script.js
@@ -1,10 +1,23 @@
 function main() {
-  const buttonGetPokemon = document.getElementById('button-get-pokemon');
-  const containerOfOptions = document.getElementById('container-of-options');
-  const imageOfPokemon = document.getElementById('image-of-pokemon');
+  // set main tags
+  const { body } = document;
+  const buttonGetPokemon = document.createElement('button');
+  buttonGetPokemon.innerText = 'Get Pokemon!';
+  buttonGetPokemon.style.display = 'block';
+  body.appendChild(buttonGetPokemon);
+
+  const containerOfOptions = document.createElement('select');
+  containerOfOptions.style.margin = '1rem 0';
+  containerOfOptions.style.display = 'block';
+  body.appendChild(containerOfOptions);
+
+  const imageOfPokemon = document.createElement('img');
+  imageOfPokemon.style.display = 'block';
+  body.appendChild(imageOfPokemon);
 
   let arrayOfPokemon;
 
+  // create options of pokemons
   const addPokemonToDOM = array => {
     array.forEach((element, index) => {
       const option = document.createElement('option');
@@ -14,6 +27,7 @@ function main() {
     });
   };
 
+  // fetch data from the Pokemon API (array of objects)
   const fetchData = () => {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
       .then(response => response.json())
@@ -25,6 +39,7 @@ function main() {
 
   buttonGetPokemon.addEventListener('click', fetchData);
 
+  // fetch the image and place it inside the image tag
   const fetchImage = event => {
     const pokemonURL = arrayOfPokemon[event.target.value].url;
     fetch(pokemonURL)
