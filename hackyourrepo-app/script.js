97020e1184ed59a22ae775f15c8afaf96721d4a6
atabek-bakh <coderonline@icloud.com>
2021-01-16 21:08:36 +0100

    testing module

--- hackyourrepo-app/script.js
+++ hackyourrepo-app/script.js
@@ -11,7 +11,34 @@ const {
   containerOfNumbers,
 } = setTags();
 
+let currentPage = 1;
 
+// the pagination is based on the source code of https://github.com/TylerPottsDev/vanillajs-pagination/blob/master/main.js/
+function setButtons(page, array) {
+  const button = document.createElement('button');
+  button.innerText = page;
+
+  if (currentPage === page) {
+    button.classList.add('active');
+  }
+  button.addEventListener('click', () => {
+    currentPage = page;
+    generateContributorsList(array, contributorsListHolder, currentPage);
+    const currentButton = document.querySelector('button.active');
+    currentButton.classList.remove('active');
+    button.classList.add('active');
+  });
+  return button;
+}
+
+function setNumberOfPages(array, wrapper) {
+  wrapper.innerHTML = '';
+  const numberOfPages = Math.ceil(array.length / 5);
+  for (let i = 1; i <= numberOfPages; i++) {
+    const button = setButtons(i, array);
+    wrapper.appendChild(button);
+  }
+}
 
 async function runMain() {
   const data = await fetchData(mainUrl);
